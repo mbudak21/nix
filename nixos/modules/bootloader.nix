02cb2c687d2ecc4e48cefd4df7eae2bd3c492d40
gabechu <mbudak21@ku.edu.tr>
2024-08-28 18:02:47 +0300

    Switched to grub from systemd bootloader.

--- nixos/modules/bootloader.nix
+++ nixos/modules/bootloader.nix
@@ -1,5 +1,9 @@
 {
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.useOSProber = true;
+  boot.loader.grub.backgroundColor = "#1e1e2e";
   boot.loader.efi.canTouchEfiVariables = true;
   boot.initrd.kernelModules = [ "amdgpu" ];
   boot.kernelParams = [ "psmouse.synaptics_intertouch=0" ]; 
